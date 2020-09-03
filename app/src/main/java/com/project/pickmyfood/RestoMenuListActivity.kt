aff2f7cfe1ffbb7802cb396f10007289768b74aa
Didin Irawan <didinirawan11@gmail.com>
2020-09-03 13:57:53 +0700

    [CREATE]DesainLayout

---
+++ app/src/main/java/com/project/pickmyfood/RestoMenuListActivity.kt
@@ -0,0 +1,11 @@
+package com.project.pickmyfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestoMenuListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resto_menu_list)
+    }
+}
